b227af72c550df0da3786dd62e1999309ae448e4
Budin Kolyandov <budin_kolyandov@abv.bg>
2019-10-23 19:23:31 +0300

    Completed problem 2 from the exercise.

---
+++ IntroductionToDbExercise/VillainNames/Program.cs
@@ -0,0 +1,46 @@
+﻿using InitialSetup;
+using System;
+using System.Data.SqlClient;
+
+namespace VillainNames
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (SqlConnection connection = new SqlConnection(Config.ConnnectionString))
+            {
+                connection.Open();
+
+                string selectVillains = $@"  SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                                             FROM Villains AS v
+                                             JOIN MinionsVillains AS mv ON v.Id = mv.VillainId
+                                             GROUP BY v.Id, v.Name
+                                             HAVING COUNT(mv.VillainId) > 3
+                                             ORDER BY COUNT(mv.VillainId)";
+
+                using (SqlCommand command = new SqlCommand(selectVillains, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+
+                            string name = reader.GetString(0);
+
+                            int count = reader.GetInt32(1);
+
+                            Console.WriteLine($"{name} - {count}");
+                        }
+                    }
+
+
+                }
+
+            }
+        }
+    }
+}
+
+
