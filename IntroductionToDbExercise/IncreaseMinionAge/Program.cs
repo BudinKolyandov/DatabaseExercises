5d3ac011308e925013362371708568d34a9bdb5d
Budin Kolyandov <budin_kolyandov@abv.bg>
2019-10-27 09:19:57 +0200

    Completed problem 8

---
+++ IntroductionToDbExercise/IncreaseMinionAge/Program.cs
@@ -0,0 +1,57 @@
+﻿using InitialSetup;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace IncreaseMinionAge
+{
+    public class Program
+    {
+        public const string SelectMinions = @"SELECT Name, Age FROM Minions";
+
+        public const string UpdateMinions = @" UPDATE Minions
+                                               SET Name = UPPER(LEFT(Name, 1)) + 
+                                               SUBSTRING(Name, 2, LEN(Name)), Age += 1
+                                               WHERE Id = @Id";
+
+        public static void Main(string[] args)
+        {
+            List<int> minionIds = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            using (SqlConnection connection = new SqlConnection(Config.ConnnectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(UpdateMinions, connection))
+                {
+                    foreach (var id in minionIds)
+                    {
+                        command.Parameters.AddWithValue("@Id", id);
+                        command.ExecuteNonQuery();
+                        command.Parameters.Clear();
+                    }
+                }
+
+                using (SqlCommand commannd = new SqlCommand(SelectMinions, connection))
+                {
+                    SqlDataReader reader = commannd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        string name = (string)reader[0];
+                        int age = (int)reader[1];
+
+                        Console.WriteLine($"{name} {age}");
+                    }
+                }
+
+            }
+
+
+        }
+    }
+}
