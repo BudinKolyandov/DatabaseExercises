e308d22931628961ae1aa309a9ba635bfe1baf8a
Budin Kolyandov <budin_kolyandov@abv.bg>
2019-11-05 10:45:55 +0200

    Made the sales database compatible with SoftUni judge system

---
+++ CodeFirstDatabase/P03_SalesDatabase/Migrations/20191105084049_ProductsAddColumnDescription.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P03_SalesDatabase.Migrations
+{
+    public partial class ProductsAddColumnDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Sales",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                maxLength: 250,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Sales",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime));
+        }
+    }
+}
