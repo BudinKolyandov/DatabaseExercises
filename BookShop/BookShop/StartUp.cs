a1dcd76c04f242c11d290484d30a2d1690b71597
Budin Kolyandov <budin_kolyandov@abv.bg>
2019-11-13 19:22:29 +0200

    Completed problems 3-6 from the advanced querying exercise.

--- BookShop/BookShop/StartUp.cs
+++ BookShop/BookShop/StartUp.cs
@@ -10,26 +10,51 @@
     public class StartUp
     {
         public static void Main(string[] args)
-        {
-            string age = Console.ReadLine();
-
+        {          
             using (var context = new BookShopContext())
             {
+                Console.WriteLine("Please enter the age restriction type you want to see:");
+                string age = Console.ReadLine();
                 var result = GetBooksByAgeRestriction(context, age);
                 Console.WriteLine(result);
             }
-
+            
             using (var context = new BookShopContext())
             {
                 var result = GetGoldenBooks(context);
                 Console.WriteLine(result);
             }
-
+            
             using (var context = new BookShopContext())
             {
                 var result = GetBooksByPrice(context);
                 Console.WriteLine(result);
             }
+                        
+            using (var context = new BookShopContext())
+            {
+                Console.WriteLine("Books that are not released in:");
+                int year = int.Parse(Console.ReadLine());
+                var result = GetBooksNotReleasedIn(context, year);
+                Console.WriteLine(result);
+            }
+
+            using (var contex = new BookShopContext())
+            {
+                Console.WriteLine("Please enter the book category you need:");
+                string input = Console.ReadLine();
+                var result = GetBooksByCategory(contex, input);
+                Console.WriteLine(result);
+            }
+
+            using (var context = new BookShopContext())
+            {
+                Console.WriteLine("Please enter the date to get the books released before it:");
+                string input = Console.ReadLine();
+                var result = GetBooksReleasedBefore(context, input);
+                Console.WriteLine(result);
+            }
+
         }
 
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
@@ -103,6 +128,72 @@
 
             return sb.ToString().TrimEnd();
         }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var titles = context.Books
+                .Where(x => x.ReleaseDate.Value.Year != year)
+                .OrderBy(x => x.BookId)
+                .Select(x => x.Title)
+                .ToList();
+
+            foreach (var title in titles)
+            {
+                sb.AppendLine(title);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string[] categories = input.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(c => c.ToLower()).ToArray();
+
+            var books = context.Books
+                .Where(x => x.BookCategories
+                .Any(c => categories.Contains(c.Category.Name.ToLower())))
+                .OrderBy(x => x.Title)
+                .Select(x => x.Title)
+                .ToList();
+
+            foreach (var book in books)
+            {
+                sb.AppendLine(book);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            DateTime releaseDate = DateTime.ParseExact(date, "dd-MM-yyyy", null);
+
+            var books = context.Books
+                .Where(x => x.ReleaseDate < releaseDate)
+                .OrderByDescending(x => x.ReleaseDate)
+                .Select(x => new
+                {
+                    x.Title,
+                    x.EditionType,
+                    x.Price
+                })
+                .ToList();
+
+            foreach (var book in books)
+            {
+                sb.AppendLine($"{book.Title} - {book.EditionType} - ${book.Price:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
 
 
     }
